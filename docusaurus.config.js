f2dd5e75901e3f0f3a0e97b1f8766f8bfafb5513
Joe Chen <geometryolife@gmail.com>
2024-09-12 17:12:23 +0800

    fix book label

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Book',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
@@ -113,8 +113,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Book',
+                to: '/docs/concepts/address',
               },
             ],
           },
